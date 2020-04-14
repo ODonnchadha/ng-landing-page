0b7f8e9eb958482860be7c7207e47ace7ab63ec6
ODonnchadha <danieldonahue1231@hotmail.com>
2020-04-14 12:09:20 -0500

    Building out the weather service.

--- NOTES.md
+++ NOTES.md
@@ -26,3 +26,31 @@
     4. Providers. Angular services prior to v6.
     5. (Bootstrap: Only present within the App.module.)
 
+- https://openweathermap.org/api
+    - Five day/three hour forecast. Via geographic cordinates.
+    - Shift + Enter to write out multi-line statements within a browser console.
+
+- mergeMap/switchMap operator(s):
+    1. https://out.stegrider.now.sh/
+        - RxJS visualizer. Great tool!
+
+    - Hijack the provided value(s) and controlliing all of the different operators. 
+    - We then return an observable. Thus, hijacked. 
+    - "Forget the original observable, use this one instead."
+
+    ```javascript
+        const { Observable, timer } = Rx;
+        const { mergeMap, tap } = RxOperators;
+
+        new Observable((observer) => {
+        observer.next(10);
+        observer.next(20);
+        }).pipe(
+            mergeMap((value) => timer(value * 100)),
+            tap((value) => console.log(value))
+        );
+    ```
+    - Whenever mergeMap listens to all observables.
+    - Whenever switchMap creates a new observable, *all* old observable(s) are deleted.
+
+    2. Why switchMap?
--- src/app/weather/forecast.service.ts
+++ src/app/weather/forecast.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ForecastService {
+  private url = "https://api.openweathermap.org/data/2.5/forecast";
 
-  constructor() { }
+  constructor(private client: HttpClient) { }
+
+  /*
+    1.  Take the Coordinates object and map/transform it.
+    2.  Create a HttpParams object to maintain our querystring via the mapped Coordinates.
+  */
+  getForecast() {
+    return this.getCurrentLocation().pipe(
+      map(coordinates => {
+        return new HttpParams()
+          .set('lat', String(coordinates.latitude))
+          .set('lon', String(coordinates.longitude))
+          .set('units', 'imperial')
+          .set('appid', '3d46ecbf6a9962da65be8bfab378bd36')
+      }),
+      switchMap(params => this.client.get(this.url, { params }))
+    );
+  }
+
+  getCurrentLocation(): Observable<Coordinates> {
+    return new Observable<Coordinates>((observer) => {
+      window.navigator.geolocation.getCurrentPosition(
+        (position) => {
+          observer.next(position.coords);
+          observer.complete();
+        }, (err) => {
+          observer.error(err);
+      })
+    });
+  }
 }
